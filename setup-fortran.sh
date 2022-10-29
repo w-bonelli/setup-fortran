191cf2187e413d0757dcd02087da3b0c80f04ecf
w-bonelli <wesbonelli@gmail.com>
2022-10-29 12:54:09 -0400

    various cleanup

--- setup-fortran.sh
+++ setup-fortran.sh
@@ -69,6 +69,12 @@ install_gcc_choco()
   export FC="gfortran"
   export CC="gcc"
   export CXX="g++"
+
+  FCDIR=/c/ProgramData/Chocolatey/bin
+  LNDIR=/c/ProgramData/Chocolatey/lib/mingw/tools/install/mingw64/bin
+  if [ -d "$FCDIR" ] && [ -f "$LNDIR/libgfortran-5.dll" ] && [ ! -f "$FCDIR/libgfortran-5.dll" ]; then
+      ln -s "$LNDIR/libgfortran-5.dll" "$FCDIR/libgfortran-5.dll"
+  fi
 }
 
 install_gcc_winlibs()
