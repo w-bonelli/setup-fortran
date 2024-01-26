5fa9e2683d0fd8baae820bb02c9c4d3f9a4aa8c6
w-bonelli <wbonelli@ucar.edu>
2024-01-26 12:01:38 -0500

    fix assert matrix generation

--- .github/compat/matrix_json_from_csv.py
+++ .github/compat/matrix_json_from_csv.py
@@ -6,34 +6,19 @@ from pathlib import Path
 csv_path = Path(sys.argv[1])  # path to CSV file
 jsn_path = Path(sys.argv[2])  # path to JSON file
 
-runners = set()
-toolchain = set()
-exclude = [
-    {"os": "macos-13", "toolchain": {"compiler": "intel"}},
-    {"os": "macos-12", "toolchain": {"compiler": "intel"}},
-    {"os": "macos-11", "toolchain": {"compiler": "intel"}},
-    {"os": "macos-13", "toolchain": {"compiler": "nvidia-hpc"}},
-    {"os": "macos-12", "toolchain": {"compiler": "nvidia-hpc"}},
-    {"os": "macos-11", "toolchain": {"compiler": "nvidia-hpc"}},
-    {"os": "windows-2022", "toolchain": {"compiler": "nvidia-hpc"}},
-    {"os": "windows-2019", "toolchain": {"compiler": "nvidia-hpc"}},
-]
+include = []
 
 with open(csv_path, "r") as csv_file:
     reader = csv.DictReader(csv_file)
     for row in reader:
         if not any(row["support"].strip()):
             continue
-        runners.add(row["runner"])
-        toolchain.add({
-            "compiler": row["compiler"],
-            "version": row["version"]
-        })
+        include.append(
+            {
+                "os": row["runner"],
+                "toolchain": {"compiler": row["compiler"], "version": row["version"]},
+            }
+        )
 
-json.dump(jsn_path, {
-    "matrix": {
-        "os": list(runners),
-        "toolchain": list(toolchain),
-        "exclude": exclude
-    }
-})
+with open(jsn_path, "w") as jsn_file:
+    json.dump({"include": include}, jsn_file)
